408d89c8c8572ac2ee733c4ce60ad05de4595605
Amelss <ameleyka@gmail.com>
2024-05-02 16:24:44 +0100

    adjusting styling blog page

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -117,7 +117,7 @@ export default function blogPosts({ blogPost }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/logo.png" />
       </Head>
-      <div className="my-3">
+      <div className="px-5 my-3">
         <h1 className="text-center text-3xl font-bold py-6">
           {blogPost.fields.title}
         </h1>
@@ -125,17 +125,17 @@ export default function blogPosts({ blogPost }) {
           <Image
             src={`https:${hero.fields.file.url}`}
             alt={heroAltText}
-            width={600}
-            height={600}
+            width={hero.fields.file.details.image.width}
+            height={hero.fields.file.details.image.height}
             className="rounded-xl mx-auto"
           />
         </div>
-        <div className="py-3 px-5">
-          <div className="xl:max-w-96">
+        <div className="py-3">
+          <div className="xl:max-w-96 xl:pl-36 text-lg">
             <h3>{author}</h3>
           </div>
 
-          <div className="flex justify-between  xl:max-w-96 text-xs py-3 text-gray-500">
+          <div className="flex justify-between  xl:max-w-[500px] text-xs py-3 text-gray-500 xl:pl-36">
             {readTime <= 1 ? (
               <p className="">Read Time: {readTime} min</p>
             ) : (
@@ -151,24 +151,24 @@ export default function blogPosts({ blogPost }) {
           <div key={index}>
             {section.sys.contentType.sys.id === "image" &&
             section.fields.image.fields.file ? (
-              <div className="py-3">
+              <div className="py-3 px-5 xl:px-0">
                 <Image
                   src={`https:${section.fields.image.fields.file.url}`}
                   alt={section.fields.altText}
-                  width={350}
-                  height={350}
+                  width={section.fields.image.fields.file.details.image.width}
+                  height={section.fields.image.fields.file.details.image.height}
                   className=" mx-auto rounded-lg"
                 />
               </div>
             ) : section.sys.contentType.sys.id === "textBlock" ? (
-              <div className="leading-relaxed px-5 py-3 xl:px-3">
+              <div className="leading-relaxed px-5 py-3 xl:px-40">
                 {documentToReactComponents(section.fields.textBlockText)}
               </div>
             ) : section.sys.contentType.sys.id === "codeBlock" ? (
-              <div className="px-5 relative">
-                <pre className="relative px-6 py-14 my-7 bg-gray-200 text-black font-mono rounded-lg overflow-x-auto xl:w-[800px]">
-                  <div className="absolute top-0 left-0 w-full h-10 bg-gray-300"></div>
-                  <code className="whitespace-pre-wrap py-2">
+              <div className="px-5 mx-auto">
+                <pre className="relative px-6 py-14 my-7 bg-gray-200 text-black font-mono rounded-lg overflow-x-auto xl:max-w-[800px] mx-auto">
+                  <div className="absolute top-0 left-0 w-full h-10 bg-gray-300 "></div>
+                  <code className="whitespace-pre-wrap py-2 text-xs xl:text-base">
                     {section.fields.codeBlockCode}
                   </code>
                   <button
