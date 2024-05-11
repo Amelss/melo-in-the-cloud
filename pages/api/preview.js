b50af3db6b8e8ff90b282c4de124c41a236a3be2
Amelss <ameleyka@gmail.com>
2024-05-11 23:12:48 +0100

    editing preview mode

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -1,6 +1,9 @@
 export default function handler(req, res) {
-    const { slug } = req.query
+    const { secret, slug } = req.query
     console.log("slug:", slug);
+    if (secret !== process.env.CONTENTFUL_PREVIEW_SECRET || !slug) {
+        return res.status(401).json({message: 'Invalid token'})
+    }
     res.setPreviewData({})
-    res.redirect(`/blogs/${slug}?preview=true`)
+    res.redirect(`/blogs/${slug}`)
 }
