78b1d8caa1f8f944c9dac4c429aba3f286bf0f76
Amelss <ameleyka@gmail.com>
2024-05-18 13:52:57 +0100

    adjusting title for mobile

--- components/FeaturedBlogCard.js
+++ components/FeaturedBlogCard.js
@@ -16,9 +16,17 @@ export default function FeaturedBlogCard({ main }) {
               alt={heroAltText}
               width={1366}
               height={768}
-              className="rounded-3xl "
+              className="rounded-3xl"
             />
-            <div className="absolute bottom-14 px-3 backdrop-blur-2xl w-[340px] lg:w-[800px] xl:w-[1000px] 2xl:w-[1300px] ml-4 py-2 lg:py-8 rounded-xl border-2 border-white border-opacity-25">
+
+            <div className="block lg:hidden">
+              <h1 className="text-xs px-2 pt-3">Featured</h1>
+              <Link
+                href={`/${process.env.NEXT_PUBLIC_BASE_PATH || ""}/${slug}`}>
+                <h1 className="font-bold px-2 pt-1 text-lg mb-5">{title}</h1>
+              </Link>
+            </div>
+            <div className="absolute bottom-14 px-3 backdrop-blur-2xl w-[340px] lg:w-[800px] xl:w-[1000px] 2xl:w-[1300px] ml-4 py-2 lg:py-8 rounded-xl border-2 border-white border-opacity-25 featured">
               <h1 className="font-bold text-white text-xs">Featured</h1>
               <h1 className="font-bold text-white text-md lg:text-3xl pt-1 xl:py-3">
                 {title}
